192930f15571d5a44724e3fcb3b05a544509d6ac
Issa laye KANE <issa.k@deally.fr>
2025-03-26 07:27:21 +0000

    first commit

--- lib/utils/requette-by-dii.dart
+++ lib/utils/requette-by-dii.dart
@@ -4,8 +4,8 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 // ignore: constant_identifier_names
-const String BASE_URL = "http://localhost:6800/v1/api";
-const String BASE_URL_ASSET = "http://localhost:6800/swaped-file/";
+const String BASE_URL = "https://api-form.nataal.shop/v1/api";
+const String BASE_URL_ASSET = "https://api-form.nataal.shop/swaped-file/";
 // const String BASE_URL = "https://481e-41-82-214-214.ngrok-free.app/v1/api";
 
 Future getResponse({
